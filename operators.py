eeaff893cd57ffab58f885e4192843fe3fcd5d1c
mariagt267 <zS18021334@estudiantes.uv.mx>
2021-03-26 17:32:38 +0000

    operators.py

---
+++ operators.py
@@ -0,0 +1,22 @@
+age1 = 9.0
+age2 = 20.0
+
+print "age1 =", age1
+print "age2 =", age2
+
+sumage = age1 + age2;
+print " suma (+) :", sumage
+print " resta (-) :", age1 - age2
+print  " mul (*)", age1 * age2 
+print "div (/))   : ", age1 / age2
+print "reta (%)    :", age2 % age1 
+
+message = "Today was a beatiful dat"
+firstname = "sito"
+lastname = "concepcion"
+
+print message 
+print firstname 
+print lastname
+
+print "Suma strings (+)", message + " "+ firstname + " " + lastname
