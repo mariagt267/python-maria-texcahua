904a2dfcb074897c1c032f7904d1939c6370ec95
mariagt267 <zS18021334@estudiantes.uv.mx>
2021-03-26 16:20:55 +0000

    DESCRIPCIONES

---
+++ variables.py
@@ -0,0 +1,12 @@
+year = 2021;  
+sentence= "my name is Maria";
+sarah, bob, mike = 16,20,25;
+#name,age= "Maria", 24;
+#mary= fer=20;
+#PI = 3.1416;
+#e = 2.71;
+
+#display values 
+print("year",year);
+print(sentence);
+print("Sara:", sarah,"bob", bob, "Mar:i::", mike);
