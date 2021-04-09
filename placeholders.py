4557c9582da47b0eff91f49d54f77e15dcc1c062
mariagt267 <zS18021334@estudiantes.uv.mx>
2021-04-09 16:43:36 +0000

    placeholders

---
+++ placeholders.py
@@ -0,0 +1,14 @@
+# placeholders.py
+
+name = "mary"
+age = "19"
+city = "orizaba"
+state = "veracruz"
+country = "mexico"
+
+message = "My name is %s, im %s years old \n"
+print message% (name, age);
+
+message = "\t My name is %s \n\t I am %s years old \n\t I am from %s %s, %s"
+print message% (name, age, city, state, country)
+
