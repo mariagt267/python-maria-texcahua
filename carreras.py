17dba1de9491edfe7414ae309e077fb5b3a21fac
mariagt267 <zS18021334@estudiantes.uv.mx>
2021-04-16 15:20:27 +0000

    Carreras usbi

---
+++ carreras.py
@@ -0,0 +1,14 @@
+#Maria
+#carreras.py
+
+carreras = input("input carreras 1,2,3,4 :")
+if (carreras == 1):
+    print ("LSCA")
+elif (carreras == 2 ):
+    print ("IS")
+elif (carreras == 3 ):
+    print ("LAE")
+elif (carreras == 4):
+    print ("CONTA")
+else:
+  print ("ERROR")
