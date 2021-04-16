ca841219992b4962bf8e9ecba234770dd09c820f
mariagt267 <zS18021334@estudiantes.uv.mx>
2021-04-16 16:12:45 +0000

    Listas

---
+++ examen-demo1/datos.py
@@ -0,0 +1,10 @@
+#Maria.Concepcion
+# datos.py
+
+nombre = "MARIA CONCEPCION GONZALEZ TEXCAHUA "
+matricula = "zs18021334"
+carrera = "LSCA"
+
+message = " Nombre: %s \n Matricula: %s \n Carrera: %s"
+print (message%(nombre, matricula, carrera))
+
---
+++ examen-demo1/listas.py
@@ -0,0 +1,33 @@
+#Maria.Concepcion
+#listas.py
+
+LSCA = ["L","I","C",".","S","I","S","T","E","M","A","S","C","O","M","P","U","T","A","C","I","O","N","A","L","E","S","A","D","M","I","N","I","S","T","R","A","T","I","V","O","S"]
+
+IS = ["I","N","G","E","N","I","E","R","I","A","D","E","S","O","F","T","W","A","R","E"]
+
+GESTION = ["G","E","S","T","I","O","N"]
+
+ADMIN = ["A","D","M","I","N","I","S","T","R","A","C","I","O","N"]
+
+CONT = ["C","O","N","T","A","B","I","L","I","D","A","D"]
+
+a = len(LSCA)
+b = len(IS)
+c = len(GESTION)
+d = len(ADMIN)
+e = len(CONT)
+
+print (LSCA)
+print ("tamaño = ",a)
+
+print (IS)
+print ("tamaño = ",b)
+
+print (GESTION)
+print ("tamaño = ",c)
+
+print (ADMIN)
+print ("tamaño =",d)
+
+print (CONT)
+print ("tamaño = ",e)
