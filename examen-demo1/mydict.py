e5a382799c460ff9c5d8b4c6fa141cf152c66458
mariagt267 <zS18021334@estudiantes.uv.mx>
2021-04-21 17:01:59 +0000

    add examen demo1

---
+++ examen-demo1/mydict.py
@@ -0,0 +1,11 @@
+#Maria_Concepcion:wq
+#mydict.py
+
+carreras = {
+        "LSCA":"lic en sistemas computacionales administrativos",
+        "IS":"ingenieria de software",
+        "LAE":"lic en administracion de empresas"
+        }
+
+print (carreras)
+print ("longitud: " ,len(carreras))
